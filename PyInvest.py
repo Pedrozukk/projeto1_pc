dcd9a69e9fecbcdc6b0ec0847e3d46d1e272ce37
Pedrozukk <pedrohenriquevince@gmail.com>
2026-03-11 22:11:17 -0300

    Código inicial do projeto 1
    Tarefas realizadas: importação da bibliotecas, entrada de dados, cálculos até o item poupança.

---
+++ PyInvest.py
@@ -0,0 +1,40 @@
+import math
+import random
+import datetime
+import statistics
+import locale
+
+locale.setlocale(locale.LC_ALL,'pt_BR.UTF-8')
+
+#entrada 
+
+capital = float(input('Capital inicial: '))
+aporte = float(input('Aporte inicial: '))
+meses = float(input('Prazo(meses): '))
+cdi_anual = float(input('CDI anual (%): '))/100
+perc_cdb = float(input('Percentua do CDI - CDB (%): '))/100
+perc_lci = float(input('Percentual do CDI - LCI (%): '))/100
+taxa_fii = float(input('Rentabilidade do FII (%): '))/100
+meta = float(input('Meta finaceira (R$): '))
+
+#conversão do CDI
+cdi_mensal = math.pow((1+cdi_anual), 1/12) - 1
+
+#total investido
+total_investido = capital + (aporte * meses)
+
+#CDB
+taxa_cdb = cdi_mensal * perc_cdb
+montante_cdb = (capital * math.pow((1+taxa_cdb), meses))+(aporte * meses)
+lucro_cdb = montante_cdb - total_investido
+montante_cdb_liquido = total_investido + (lucro_cdb * 0.85)
+
+#LCI/LCA
+taxa_lci = cdi_mensal * perc_lci
+montante_lci = (capital * math.pow((1+taxa_lci), meses)) +(aporte * meses)
+
+#Poupança
+taxa_poupanca = 0.005 
+montate_poupanca = (capital * math.pow((1+taxa_poupanca), meses)) + (aporte * meses)
+
+#

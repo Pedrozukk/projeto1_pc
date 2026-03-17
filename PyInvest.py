ca67978a87e3cd18ce28233975209c31cfa2751a
Pedrozukk <pedrohenriquevince@gmail.com>
2026-03-17 19:17:24 -0300

    Add files via upload

--- PyInvest.py
+++ PyInvest.py
@@ -35,7 +35,7 @@ montante_lci = (capital * math.pow((1+taxa_lci), meses)) +(aporte * meses)
 
 #Poupança
 taxa_poupanca = 0.005 
-montate_poupanca = (capital * math.pow((1+taxa_poupanca), meses)) + (aporte * meses)
+montante_poupanca = (capital * math.pow((1+taxa_poupanca), meses)) + (aporte * meses)
 
 #FII
 montante_fii = (capital * math.pow((1+taxa_fii), meses)) + (aporte * meses) 
@@ -55,10 +55,48 @@ media_fii = st.mean((simu1, simu2, simu3, simu4, simu5))
 mediana_fii = st.median((simu1, simu2, simu3, simu4, simu5))
 desvio_fii = st.stdev((simu1, simu2, simu3, simu4, simu5))
 
+#GRAFICO
+blocos_cdb = int(montante_cdb_liquido )/ 1000
+blocos_lci = int(montante_lci )/ 1000
+blocos_poupamca = int(montante_poupanca )/ 1000
+blocos_fii = int(montante_fii )/ 1000
 
 #data
 data_inicial = datetime.date.today()
 data_final = data_inicial + datetime.timedelta(days= meses * 30)
 
+#Formatar datas
+data_simulacao = data_inicial.strftime('%d/%m/%Y')
+data_resgate_fmt = data_final.strftime('%d/%m/%Y')
+
 #meta
 meta_atingida = media_fii >=meta
+
+print('-' * 40)
+print(" PyInvest Simulador de Investimentos")
+print('=' * 40)
+print(f'Data da simulacao:{data_inicial.strftime("%d/%m/%Y")}')
+print(f'Data de resgaste:{data_final.strftime("%d/%m/%Y")}')
+print('=' * 40)
+print('       ')
+print(f'Total investido:{locale.currency(total_investido, grouping=True)}')
+print('       ')
+print('--- RESULTADOS FINANCEIROS ---')
+print(f'CDB: {locale.currency(montante_cdb_liquido, grouping=True)}')
+print(f'{'█'* int(montante_cdb_liquido / 1000)}')
+print('       ')
+print(f'LCI:{locale.currency(montante_lci, grouping=True)}')
+print(f'{'█'* int(montante_lci / 1000)}')
+print('       ')
+print(f'Poupanca:{locale.currency(montante_poupanca, grouping=True)}')
+print(f'{'█'* int(montante_poupanca / 1000)}')
+print('       ')
+print(f'FII(media:{locale.currency(media_fii, grouping=True)}')
+print(f'{'█'* int(media_fii / 1000)}')
+print('       ')
+print('--- ESTATISTICAS FII ---')
+print(f'Mediana:{locale.currency(mediana_fii, grouping=True)}')
+print(f'Desvio padrao:{locale.currency(desvio_fii, grouping=True)}')
+print('       ')
+print(f'Meta atingida?:{media_fii >= meta}')
+print('='*40)
